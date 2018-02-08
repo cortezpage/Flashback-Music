1c7552d1aa5d21ca1cf79c4d730775f5bf5eb5e5
Athena Hosek <ahosek@ucsd.edu>
2018-02-07 21:02:15 -0800

    Moved code out of MainActivity and into a MusicPlay activity

--- FlashbackMusic/app/src/main/java/com/example/cse110/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/com/example/cse110/flashbackmusic/MainActivity.java
@@ -1,123 +1,30 @@
 package com.example.cse110.flashbackmusic;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.provider.MediaStore;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
-
-    private MusicPlayer musicPlayer;
-    private SharedPrefHelper sharedPrefHelper;
-    private SharedPreferences.Editor dataEditor;
-    private String [] song_data;
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        musicPlayer.destroy();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        song_data = new String[0];
+        Button switchButton = (Button) findViewById(R.id.button_play_music);
 
-        Context context = this.getApplicationContext();
-        String filename = "com.example.cse110.flashbackmusic.song_data_preferences";
-        SharedPreferences sharedPref = context.getSharedPreferences(filename, Context.MODE_PRIVATE);
-        dataEditor = sharedPref.edit();
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMusicPlay();
+            }
+        });
+    }
 
-        sharedPrefHelper = new SharedPrefHelper(sharedPref, dataEditor);
-        sharedPrefHelper.validateData();
-
-        Map<String, ?> allEntries = sharedPref.getAll();
-        song_data = new String[allEntries.size()];
-        int index = 0;
-        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
-            Log.i("Read song data:", entry.getKey() + ": " + entry.getValue().toString());
-            song_data[index] = entry.getValue().toString();
-            index++;
-        }
-
-        musicPlayer = new MusicPlayer(this.getResources());
-        musicPlayer.loadSongs(song_data);
-
-        // Link the "play" button with the play() method from the music player
-        Button playButton = (Button) findViewById(R.id.button_play);
-        playButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        musicPlayer.play();
-                    }
-                }
-        );
-
-        // Link the "pause" button with the pause() method from the music player
-        Button pauseButton = (Button) findViewById(R.id.button_pause);
-        pauseButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        musicPlayer.pause();
-                    }
-                }
-        );
-
-        // Link the "reset" button with the reset() method from the music player
-        Button resetButton = (Button) findViewById(R.id.button_reset);
-        resetButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        musicPlayer.reset();
-                    }
-                }
-        );
-
-        // Link the "previous" button with the playPreviousSong() method from the music player
-        Button previousButton = (Button) findViewById(R.id.button_previous);
-        previousButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        musicPlayer.goToPreviousSong();
-                    }
-                }
-        );
-
-        // Link the "next" button with the playNextSong() method from the music player
-        Button nextButton = (Button) findViewById(R.id.button_next);
-        nextButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        musicPlayer.goToNextSong();
-                    }
-                }
-        );
-
-        Button likeButton = (Button) findViewById(R.id.button_like);
-        likeButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        musicPlayer.changeCurrentLikeStatus();
-                        int currID = musicPlayer.getCurrentMediaID();
-                        String currSong = musicPlayer.getCurrentString();
-                        sharedPrefHelper.writeSongData(currID, currSong);
-                        sharedPrefHelper.applyChanges();
-                    }
-                }
-        );
+    public void launchMusicPlay () {
+        Intent intent = new Intent(this, MusicPlay.class);
+        startActivity(intent);
     }
 }
--- FlashbackMusic/app/src/main/java/com/example/cse110/flashbackmusic/MusicSelection.java
+++ FlashbackMusic/app/src/main/java/com/example/cse110/flashbackmusic/MusicSelection.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MusicSelection extends AppCompatActivity {
 
@@ -13,15 +14,13 @@ public class MusicSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_selection);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button back = (Button) findViewById(R.id.button_exit_music_selection);
+
+        back.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view){
+                finish();
             }
         });
     }
