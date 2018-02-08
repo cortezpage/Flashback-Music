be06d6ad8935f3ef9d66bf2d51b8506229a65d40
Athena Hosek <ahosek@ucsd.edu>
2018-02-08 13:24:31 -0800

    Changed all Activity files to include Activity in their names

--- FlashbackMusic/app/src/main/java/com/example/cse110/flashbackmusic/AlbumSelectionActivity.java
+++ FlashbackMusic/app/src/main/java/com/example/cse110/flashbackmusic/AlbumSelectionActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class AlbumSelection extends AppCompatActivity {
+public class AlbumSelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
